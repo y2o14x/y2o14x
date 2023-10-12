713d4e7fbad65d74b2fb62b57a05c1ba6d030204
y2o'14x <115692514+y2o14x@users.noreply.github.com>
2023-10-12 09:25:56 +0200

    Update recursivité.py

--- recursivité.py
+++ recursivité.py
@@ -47,13 +47,45 @@ def inversion_v2(T, i=0):
     >>> T
     [3, 2, 1, 0]
     """
-    if i == len(T) // 2: return
+    n = len(T)
+    if i == n // 2: return
     #
+    T[i], T[n - 1 - i] = T[n - 1 - i], T[i]
+    inversion_v2(T, i + 1)
 
+# exercice 3
 
+def mediane(T):
+    """
+    >>> T = [0, 1, 2, 3]
+    >>> mediane(T)
+    1.5
+    >>> T = [0, 1, 2, 3, 4]
+    >>> mediane(T)
+    2 
+    """
+    n = len(T)
+    if n <= 2 : return (T[0] + T[n - 1]) / 2 
+    #
+    return mediane(T[1:n - 1])
+    
+# exercice 4
+
+def bin_s(T, e):
+    if len(T) == 1: return T[0] == e 
+    #
+    indice_milieu = len(T) // 2
+    milieu = T[indice_milieu]
+    if T[milieu] == e: return True
+    if T[milieu] < e:
+        nv_tab = T[indice_milieu:]
+    else:
+        nv_tab = T[0:indice_milieu]
+    return bin_s(nv_tab, e)
 
 if __name__ == "__main__":
     import doctest
+    doctest.testmod()
     doctest.testmod()
 
 
