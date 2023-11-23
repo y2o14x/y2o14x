a5abe359e26f1f0ffa09d1b1c228fb4321eaa292
y2o'14x <115692514+y2o14x@users.noreply.github.com>
2023-11-23 09:55:52 +0100

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+def fusion(tab1, tab2):
+    resultat = []
+    #
+    i1 = i2 = 0
+    while i1 < len(tab1) and i2 < len(tab2):
+        pass
+    
+    if i1 == len(tab1):
+        pass
+    else:
+        pass
+    
